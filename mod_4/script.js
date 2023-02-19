9b93344feabd89b04546b82a299a18662329286e
Adam Sayani <asayani1@jh.edu>
2023-02-18 21:50:34 -0500

    mod 4, implemented part 2 and bonus

--- mod_4/script.js
+++ mod_4/script.js
@@ -33,32 +33,96 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
+(function() {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  // STEP 10:
+  // Loop over the names array and say either 'Hello' or "Good Bye"
+  // using either the helloSpeaker's or byeSpeaker's 'speak' method.
+  // See Lecture 50, part 1
+  for (var person in names) {
 
-// STEP 10:
-// Loop over the names array and say either 'Hello' or "Good Bye"
-// using either the helloSpeaker's or byeSpeaker's 'speak' method.
-// See Lecture 50, part 1
-for (/* fill in parts of the 'for' loop to loop over names array */) {
+    // STEP 11:
+    // Retrieve the first letter of the current name in the loop.
+    // Use the string object's 'charAt' function. Since we are looking for
+    // names that start with either upper case or lower case 'J'/'j', call
+    // string object's 'toLowerCase' method on the result so we can compare
+    // to lower case character 'j' afterwards.
+    // Look up these methods on Mozilla Developer Network web site if needed.
+    var firstLetter = names[person].charAt(0).toLowerCase();
+    //console.log("firstletter is: " + firstLetter);
 
-  // STEP 11:
-  // Retrieve the first letter of the current name in the loop.
-  // Use the string object's 'charAt' function. Since we are looking for
-  // names that start with either upper case or lower case 'J'/'j', call
-  // string object's 'toLowerCase' method on the result so we can compare
-  // to lower case character 'j' afterwards.
-  // Look up these methods on Mozilla Developer Network web site if needed.
-  // var firstLetter =
-
-  // STEP 12:
-  // Compare the 'firstLetter' retrieved in STEP 11 to lower case
-  // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
-  // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
-  // name in the loop.
-  if (/* fill in condition here */) {
-    // byeSpeaker.xxxx
-  } else {
-    // helloSpeaker.xxxx
+    // STEP 12:
+    // Compare the 'firstLetter' retrieved in STEP 11 to lower case
+    // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
+    // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
+    // name in the loop.
+    if (firstLetter == "j") {
+      byeSpeaker.speak(names[person]);
+    } else {
+      helloSpeaker.speak(names[person]);
+    }
   }
-}
+
+
+  //=====PART 2: use a map function to create greetings
+  //function to be used by map method, to determine which greeing to use and return as a string with the name
+  var simpleMapFunc = function (name) {
+    var firstLetter = name.charAt(0).toLowerCase();
+    if (firstLetter == "j") {
+      return byeSpeaker.speakSimple(name);
+    } else {
+      return helloSpeaker.speakSimple(name);
+    }
+  }
+
+  var greetedNames = names.map(simpleMapFunc);
+
+  //debug
+  //console.log(greetedNames);
+  console.log("\n\n======PART 2: ");
+  for( var human in greetedNames){
+    console.log(greetedNames[human]);
+  }
+
+
+
+  //=====PART 3: Bonus using reduce
+  console.log("\n\n======Part 3 BONUS");
+  var initialValue = {hello: [], bye: []};
+  names.reduce( 
+    (accumulatingObject, currentValue) => {
+      //console.log("curval: " + currentValue );
+
+
+      //console.log(currentValue.charAt(0).toLowerCase() );
+      if(currentValue.charAt(0).toLowerCase() == "j"){
+        //console.log("about to push to bye: " + currentValue);
+        //var count = initialValue.bye.push(currentValue);
+        var count = initialValue.bye.push(byeSpeaker.speakSimple(currentValue));
+        //console.log("count of bye is now: " + count);
+      } else {
+        //console.log("about to push to hello: " + currentValue);
+        //var count = initialValue.hello.push(currentValue);
+        var count = initialValue.hello.push(helloSpeaker.speakSimple(currentValue));
+        //console.log("count of hello is now: " + count);
+      }
+    }, initialValue
+  );
+
+  //output people in the hello array
+  for(var helloPerson in initialValue.hello){
+    console.log(initialValue.hello[helloPerson]);
+  }
+
+  //output people in the goodbye array
+  for(var byePerson in initialValue.bye){
+    console.log(initialValue.bye[byePerson]);
+  }
+
+
+//console.log(initialValue.hello);
+
+//console.log(initialValue.bye);
+
+})();
