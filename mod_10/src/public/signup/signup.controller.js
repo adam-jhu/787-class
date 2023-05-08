9f5782710c2578fc9425d6a7da959baf337208f7
Adam Sayani <asayani1@jh.edu>
2023-05-07 23:28:24 -0400

    appears to be mostly working, needs cleanup

--- mod_10/src/public/signup/signup.controller.js
+++ mod_10/src/public/signup/signup.controller.js
@@ -82,15 +82,19 @@
             var favDish = shortname;
             var numPart = favDish.match(/[0-9]+/);
             var numIndex = favDish.indexOf(numPart);
-            console.log('numeric index: ', numIndex);
+            //console.log('numeric index: ', numIndex);
             if(! (numIndex === -1)) {
                 var letterPart = favDish.substring(0, numIndex);
-                console.log("favDish letter: " + letterPart + ", numeric:" + numPart);
+                
                 numPart = numPart - 1; //subtract 1, zero based menu numbering/arry numbering
-                console.log('subtracted number: ', numPart);
+                console.log("favDish letter: " + letterPart + ", numeric:" + numPart);
+                //console.log('subtracted number: ', numPart);
                 //check whether this is actually a valid menu item
                 //var single = $ctrl.allItems[letterPart]; //works
-                var single = $ctrl.allItems[letterPart].menu_items[numPart-1];
+
+                //the (number part of the shortname) -1 = the array index for the category
+                var single = $ctrl.allItems[letterPart].menu_items[numPart];
+                //var single = $ctrl.allItems[letterPart].menu_items[shortname];
                 console.log('single', single);
 
                 return single;
@@ -108,12 +112,20 @@
     //code to add custom validation to the form, demo code copied from:
     // https://docs.angularjs.org/guide/forms#custom-validation
     angular.module('public')
-    .directive('validatedish', function() {
+    .directive('validatedish', ['MenuService',  function(MenuService) {
         return {
           require: 'ngModel',
           //controller: SingupController,
           link: function(scope, elm, attrs, ctrl) {
+            var allItems = {};
+            MenuService.getAllMenuItems().then(function (response){
+                console.log('in directive then, resp.d: ', response);
+                allItems = response;
+            });
+            console.log('in directive, all items: ', allItems);
+
             ctrl.$validators.validatedish = function(modelValue, viewValue) {
+
 
                 console.log('model value: ', modelValue);
                 console.log('view value: ',viewValue);
@@ -126,11 +138,22 @@
                         var letterPart = viewValue.substring(0, numIndex);
                         console.log("viewmodel letter: " + letterPart + ", numeric:" + numPart);
                         //check whether this is actually a valid menu item
-                        //console.log('all items in dir: ', ctrl.allItems);
-                        if (viewValue == "L1") {
-                            // it is valid
-                            return true;
-                          }
+                        console.log('all items in dir: ', ctrl.allItems);
+                        //only attempt to read if the letter partat least matches something
+                        if(allItems[letterPart]){
+                            var singleItem = allItems[letterPart].menu_items[numPart-1];
+                            console.log('singleItem', singleItem);
+    
+                            if (singleItem) {
+                                // it is valid
+                                return true;
+                              }
+                        }
+                        else{
+                            console.log('not a valid letter, dish invalid');
+                            return false;
+                        }
+
 
                     }
                 }
@@ -140,7 +163,7 @@
             };
           }
         };
-      });
+      }]);
 
     
     })();
